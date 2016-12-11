88d0e361e78072a41d861bff79c76d904cfb4734
pieter <pieter557@gmail.com>
2016-12-11 10:28:21 +0100

    day 11 :-1:

---
+++ ConsoleApplication2/Day11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2 {
+	class Day11 {
+		internal static void part1() {
+			/*
+			 * Input:
+				 * The first floor contains a polonium generator, a thulium generator, a thulium-compatible microchip, a promethium generator, 
+				 * a ruthenium generator, a ruthenium-compatible icrochip, a cobalt generator, and a cobalt-compatible microchip.
+				 * The second floor contains a polonium-compatible microchip and a promethium-compatible microchip.
+				 * The third floor contains nothing relevant.
+				 * The fourth floor contains nothing relevant.
+				 * 
+				 * 10 items to move to floor 4. 8 from floor 1 and 2 from floor 2
+				 * For each items you need 2 moves to bring it a floor up.
+				 * -3 for last 2 items each floor,  no need to go down afterwards
+				 * 16-3+20-3+20-3 = 47
+			 * 
+			 */
+		}
+		internal static void part() {
+			/*
+			 * 4 extra items:
+			 * 3*4*2 = 24 extra moves => 47+24 = 71
+			 */
+		}
+	}
+}
